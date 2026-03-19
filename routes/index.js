d0f565c7a572589380408fdf5c430302e64f24ed
Brice Norton Hennelly <bricenorton@fas.harvard.edu>
2026-03-19 11:12:34 -0400

    Updates to adding mongoose and mongoDB for listing all meals on the home page and displaying one meal when clicked on from home page.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const Meal = require('../models/mealModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'What Did I Eat Today?',
-    foodDiary: req.app.locals.foodDiary
-  });
+router.get('/', async function(req, res, next) {
+  try {
+    const meals = await Meal.find();
+
+
+
+    res.render('index', {
+      title: 'What Did I Eat Today?',
+      meals: meals.map((meal) => ({
+        id: meal._id.toString(),
+        meal: meal.mealname,
+      })),
+    });
+  } catch (err) {
+    next(err);
+  }
 });
+
+
 
 module.exports = router;
--- routes/meals.js
+++ routes/meals.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Meal = require('../models/mealModel');
 
-/* GET single meal by id. */
-router.get('/:mealid', (req, res, next)=>{
-  const id = parseInt(req.params.mealid);
-  const selectedMeal = req.app.locals.meals.find(m => m.id === id);
-  const mealTitle = selectedMeal.meal;
+/* GET single meal by id. with Mongoose */
+router.get('/:mealid', async function(req, res, next) {
+  // if the meal id isn't valid disply 'No meal found'
+  if (!mongoose.isValidObjectId(req.params.mealid)) {
+    return res.status(404).render('meal', {
+      title: 'No meal found',
+      meal: null
+    });
+  }
+
+  let meal = null;
+  try {
+    meal = await Meal.findById(req.params.mealid);
+  } catch (err) {
+    console.error("Error getting meal from our database", err);
+    return next(err);
+  }
+  // If the meal id doesn't exists for some reason, display 'not found meessage'
+  if (!meal) {
+    return res.status(404).render('meal', {
+      title: 'No meal found',
+      meal: null
+    });
+  }
+  // otherwise display meal
   res.render('meal', {
-    title: mealTitle,
-    description: selectedMeal.description,
-    plateImageURL: selectedMeal.plateImageURL
+    title: meal.mealname,
+    meal: meal
   });
 });
 
-/* POST new photo using Mongo. */
+
+/* POST new meal using Mongo. */
 router.post('/', async function(req, res, next) { 
   const mealData  = {
     mealname: new Date().toDateString() + ": " + req.body.mealType,
